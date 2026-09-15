d394a885486ffffdbd8a4c8b34dc00434e81375a
Urian1983 <urianr@gmail.com>
2026-09-15 10:46:34 +0200

    modified pom.xml for generating separated ,jar files for structured package

--- src/main/java/model/DiscountCalculator.java
+++ src/main/java/model/DiscountCalculator.java
@@ -3,5 +3,6 @@ package model;
 public class DiscountCalculator {
     public float calculateDiscount (float discountPrice, float originalPrice){
         return discountPrice*(100/originalPrice);
+
     }
 }
